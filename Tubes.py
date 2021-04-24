abb870a998f5bd496f277da4cf9510750bdb2244
stefanus-lamlo <73290494+stefanus-lamlo@users.noreply.github.com>
2021-04-24 17:09:59 +0700

    tubes dan database

---
+++ Tubes.py
@@ -0,0 +1,107 @@
+import csv
+import re
+
+fungsi =    {'tambahTugas': r'.*\s*tambah tugas*',
+            'tampilTugas': r'tampilkan*',
+            'tampilDeadline': r'.*cube.*(\d+)',
+            'updateTanggal': r'.*cube.*(\d+)',
+            'done': r'.*cube.*(\d+)'
+                }
+
+def bacaDB():
+    with open('databasenew.csv',newline='') as DB:
+        reader = csv.reader(DB)
+        listMatkul = []
+        for row in reader:
+            listMatkul.append(row)
+    return listMatkul
+
+def tambahTugas(tanggal, matkul, jenis, topik):
+    with open('databasenew.csv','a',newline='') as DB:
+        writer = csv.writer(DB)
+        newTugas = [str(len(arrayDB)),tanggal,matkul,jenis,topik,"FALSE"]
+        arrayDB.append(newTugas)
+        writer.writerow(newTugas)
+
+def tampilTugas():
+    for row in arrayDB:
+        if(row[5]=="FALSE"):
+            print(row)
+
+def tampilDeadline(matkul):
+    for row in arrayDB:
+        if(row[2]==matkul):
+            print(row[1])
+
+def updateTanggal(id,tanggal):
+    for row in arrayDB:
+        if(row[0]==str(id)):
+            row[1]=tanggal
+    with open('databasenew.csv', 'w', newline='') as file:
+        mywriter = csv.writer(file, delimiter=',')
+        mywriter.writerows(arrayDB)
+
+def done(id):
+    for row in arrayDB:
+        if(row[0]==str(id)):
+            row[5]='TRUE'
+    with open('databasenew.csv', 'w', newline='') as file:
+        mywriter = csv.writer(file, delimiter=',')
+        mywriter.writerows(arrayDB)
+
+def tampilHelp():
+    print('''
+Udang Mayones merupakan salah satu menu favorit di restoran chinese atau restoran seafood. 
+Udang digoreng renyah dengan balutan tepung dipadukan dengan campuran saus mayones yang segar. 
+Ternyata menu restoran ini sangat mudah dibuat asalkan tahu tips & tricksnya. 
+Cocok dijadikan camilan atau lauk untuk anak-anak dan keluarga di rumah.
+
+Langkah:
+1.  Belah udang menjadi potongan butterfly
+2.  Masukkan jahe halus, baking soda, putih telur, garam, merica, dan sedikit tepung maizena. 
+    Aduk rata lalu diamkan 10-15 menit di suhu ruangan
+3.  Buat dynamite mayo original. 
+    Campurkan mayones, air jeruk nipis, susu kental manis, dan madu. Aduk rata, tambahkan sedikit garam
+4.  Buat dynamite mayo pedas dengan menambahkan saus sambal dan bawang putih. Aduk rata
+5.  Setelah udang di marinasi, masukkan ke dalam tepung maizena, aduk rata hingga seluruh permukaannya terlapisi, lalu saring
+6.  Goreng udang hingga matang, angkat lalu tiriskan
+7.  Campur udang goreng dengan dynamite mayo, aduk rata
+8.  Siapkan bihun jagung goreng, garnish dengan biji wijen hitam dan daun bawang. Udang Mayones siap disajikan
+
+    ''')
+
+def chat():
+    command = input().lower()
+    while (command != "STOP YA BEROW"):
+        reply(command)
+        command = input().lower()
+
+def reply(command):
+    for key,value in fungsi.items():
+        intent = key
+        pattern = value
+        found_match = re.match(pattern, command)
+        if found_match and intent =='tambahTugas':
+            return tambahTugas('4/24/2021','OOP','Tubes','engimon')
+        elif found_match and intent =='tampilTugas':
+            return tampilTugas()
+        elif found_match and intent == 'tampilDeadline':
+            return tampilDeadline(matkul)
+        elif found_match and intent == 'updateTanggal':
+            return updateTanggal(28, "4/28/2021")
+        elif found_match and intent == 'done':
+            return done(25)
+    else:
+        print("punten kang aing teu ngertos maneh ngomong naon")
+
+
+
+arrayDB = bacaDB()
+tambahTugas('4/24/2021','OOP','Tubes','engimon')
+tampilTugas()
+updateTanggal(28, "4/28/2021")
+tampilTugas()
+done(25)
+tampilTugas()
+tampilHelp()
+chat()
